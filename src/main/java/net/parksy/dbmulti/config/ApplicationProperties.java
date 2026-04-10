aab6a5af44b6f476c2ac9af185ccd9ae15c1a8af
Darryl Parks <296842+djparks@users.noreply.github.com>
2026-04-10 13:47:55 -0500

    Update ApplicationProperties.java

--- src/main/java/net/parksy/dbmulti/config/ApplicationProperties.java
+++ src/main/java/net/parksy/dbmulti/config/ApplicationProperties.java
@@ -1,7 +1,20 @@
 package net.parksy.dbmulti.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Data
+@Configuration
 @ConfigurationProperties(prefix = "first", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private Datasource datasource = new Datasource();
+
+    @Data
+    public static class Datasource {
+        private String url;
+        private String driverClassName;
+        private String username;
+        private String password;
+    }
 }
